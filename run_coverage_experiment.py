:bug: Fix adaptive prediction set calculation

run_coverage_experiment.py
@@ -5,8 +5,9 @@ Conduct experiments for conformal risk control in NLG.
 # STD
 import argparse
 from datetime import datetime
+import math
 import os
-from typing import Optional, Dict
+from typing import Optional, Dict, Tuple
 
 # EXT
 from codecarbon import OfflineEmissionsTracker
@@ -37,13 +38,14 @@ DATASETS = {
     "jaen": ("ja_XX", "en_XX")
 }
 
-CALIBRATION_DATA_PATH = "./data/calibration/calibration_data.npy"
+CALIBRATION_DATA_PATH = "./data/calibration/calibration_data.npy"  # TODO: Debug
 
 # DEFAULTS
 SEED = 1234
-BATCH_SIZE = 6  # TODO: Debug 64
-NUM_BEAMS = 4  # TODO: Debug
+BATCH_SIZE = 4
 ALPHA = 0.9
+TEMPERATURE = 1
+NUM_NEIGHBORS = 100
 
 # GLOBALS
 SECRET_IMPORTED = False
@@ -77,7 +79,7 @@ def run_experiments(
     device: Device,
     data_dir: str,
     result_dir: str,
-    data_store_dir: str,
+    datastore_dir: str,
     ignore_token_ids: Tuple[int] = (1, 2),  # TODO: Double-check this default
 ):
     """
@@ -136,7 +138,7 @@ def run_experiments(
         num_centroids=num_centroids, code_size=code_size,
         num_probes=num_probes, use_quantization=use_quantization
     )  # Create empty data store
-    data_store.load(data_store_dir)  # Load in contents
+    data_store.load(datastore_dir)  # Load in contents
 
     # Init conformal calibrator
     calibrator = ConformalCalibrator(
@@ -181,17 +183,29 @@ def run_experiments(
             labels = labels[mask]
 
             # Run the non-exchangeable conformal prediction
-            distances, conformity_scores = data_store.search_k(
-                decoder_states, k=num_neighbors
-            )
-            weights = calibrator.get_weights(distances)
-            conformal_result = calibrator.compute_q_hat(
+            distances, conformity_scores = [], []
+
+            # This can be hard on memory so we do it in batches
+            bbatch_size = 1  # TODO: Debug batch_size
+            for i in range(0, len(decoder_states), bbatch_size):
+
+                batch_distances, batch_conformity_scores = data_store.search_k(
+                    decoder_states[i:i+bbatch_size, :], k=num_neighbors
+                )
+                distances.append(batch_distances)
+                conformity_scores.append(batch_conformity_scores)
+
+            distances = torch.cat(distances, dim=0) / math.sqrt(model.config.d_model)  # Normalize by hidden dim
+            conformity_scores = torch.cat(conformity_scores, dim=0).squeeze(-1)
+            weights = calibrator.compute_weights(distances)
+            conformal_results = calibrator.compute_q_hat(
                 weights, conformity_scores
             )
-            q_hat = conformal_result.q_hat
-            prediction_sets, set_sizes = calibrator.get_prediction_sets(conformity_method, prediction_sets, q_hat)
+            q_hat = conformal_results["q_hat"]
+            prediction_sets, set_sizes = calibrator.get_prediction_sets(conformity_method, predictions, q_hat)
 
             # Evaluate
+            a = 3
             ...  # TODO
 
         # Save results
@@ -206,12 +220,7 @@ if __name__ == "__main__":
         default=MODEL_IDENTIFIER
     )
     parser.add_argument(
-        "--model-path",
-        type=str,
-        default=None
-    )
-    parser.add_argument(
-        "--calibration-data-path",
+        "--datastore-dir",
         type=str,
         default=CALIBRATION_DATA_PATH
     )
@@ -227,14 +236,45 @@ if __name__ == "__main__":
         default=BATCH_SIZE
     )
     parser.add_argument(
-        "--num-beams",
-        type=int,
-        default=NUM_BEAMS
+        "--conformity-method",
+        type=str,
+        choices=("simple", "adaptive"),
+        default="adaptive"
+    )
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=TEMPERATURE
+    )
+    parser.add_argument(
+        "--num-neighbors",
+        type=float,
+        default=NUM_NEIGHBORS
     )
     parser.add_argument(
         "--alpha",
         type=int,
         default=ALPHA
+    )
+    parser.add_argument(
+        "--use-quantization",
+        action="store_true",
+        default=False
+    )
+    parser.add_argument(
+        "--num-centroids",
+        type=int,
+        default=4096
+    )
+    parser.add_argument(
+        "--code-size",
+        type=int,
+        default=64
+    )
+    parser.add_argument(
+        "--num-probes",
+        type=int,
+        default=32
     )
     parser.add_argument("--data-dir", type=str, default=DATA_DIR)
     parser.add_argument("--emission-dir", type=str, default=EMISSION_DIR)
@@ -289,15 +329,18 @@ if __name__ == "__main__":
             model_identifier=args.model,
             dataset=args.dataset,
             batch_size=args.batch_size,
+            conformity_method=args.conformity_method,
             alpha=args.alpha,
+            temperature=args.temperature,
+            num_neighbors=args.num_neighbors,
+            use_quantization=args.use_quantization,
+            num_centroids=args.num_centroids,
+            code_size=args.code_size,
+            num_probes=args.num_probes,
+            datastore_dir=args.datastore_dir,
             device=args.device,
             data_dir=args.data_dir,
             result_dir=args.result_dir,
-            source_path=source_path,
-            references_path=references_path,
-            model_path=args.model_path,
-            calibration_data_path=args.calibration_data_path,
-            wandb_run=wandb_run
         )
 
     except Exception as e:

src/conformal.py
@@ -126,31 +126,31 @@ class ConformalCalibrator:
         Tuple[float, float]
             NamedTuple of (q_hat, n_eff) where q_hat is the computed quantile and n_eff is the effective sample size.
         """
-        weights = weights.cpu().numpy()
-
         # Normalize weights
-        normed_weights = weights / (np.sum(weights) + 1)
+        normed_weights = weights / (torch.sum(weights, dim=-1, keepdim=True) + 1)
 
         # Sort by conformal score ascending since we're trying to find the smallest q
-        sorted_weights_and_scores = sorted(
-            zip(normed_weights, conformity_scores), key=lambda tpl: tpl[1]
-        )
+        sorted_conformity_scores, index = torch.sort(conformity_scores)
+        sorted_weights = torch.gather(normed_weights, -1, index)
 
         # Find the smallest q (compared to conformal scores)
         # for which the sum of corresponding weights is bigger equal than 1 - alpha
-        q_hat = np.inf
-        cumsum = 0
+        cumsum = torch.cumsum(sorted_weights, dim=-1)
+        mask = (cumsum >= (1 - self.alpha)).long()
+        threshold_index = torch.argmax(mask, dim=-1)
+        q_hat = torch.gather(sorted_conformity_scores, -1, threshold_index.unsqueeze(-1)).squeeze(-1)
 
-        for weight, score in sorted_weights_and_scores:
-            if cumsum >= 1 - self.alpha:
-                q_hat = score
-                break
+        # Make q_hat infinity in cases where the threshold was never reached
+        infinity_mask = mask.sum(dim=-1) == 0
+        q_hat[infinity_mask] = torch.inf
 
-            cumsum += weight
+        n_eff = torch.sum(weights, dim=-1) / torch.sum(weights ** 2, dim=-1)
 
-        n_eff = np.sum(weights) / np.sum(weights ** 2)
-
-        return ConformalResult(q_hat=q_hat, n_eff=n_eff, normed_weights=normed_weights)
+        return {
+            "q_hat": q_hat,
+            "n_eff": n_eff,
+            "normed_weights": normed_weights
+        }
 
     def get_prediction_sets(
         self,
@@ -164,7 +164,7 @@ class ConformalCalibrator:
         return self.prediction_set_methods[method](predictions, q_hat)
 
     @staticmethod
-    def compute_classic_prediction_set(predictions: torch.FloatTensor, q_hat: float) -> Tuple[torch.FloatTensor, int]:
+    def compute_classic_prediction_set(predictions: torch.FloatTensor, q_hat: torch.FloatTensor) -> Tuple[torch.FloatTensor, int]:
         """
         Compute the classic prediction set based on the predictions and the quantile.
 
@@ -180,15 +180,16 @@ class ConformalCalibrator:
         Tuple[torch.FloatTensor, int]
             Prediction set (in the form of a zeroed-out and re-normalized output distribution) and its size.
         """
-        # TODO: Vectorize q_hat
-        set_size = torch.sum((predictions > q_hat).long()).numpy()
-        predictions[predictions > q_hat] = 0
-        predictions /= predictions.sum()
+        q_hat = q_hat.unsqueeze(-1).repeat(1, predictions.shape[-1])
+        set_sizes = torch.sum((predictions > q_hat).long(), -1).numpy()
 
-        return predictions, set_size
+        predictions[predictions > q_hat] = 0
+        predictions /= predictions.sum(-1, keepdim=True)
+
+        return predictions, set_sizes
 
     @staticmethod
-    def compute_adaptive_prediction_set(predictions: torch.FloatTensor, q_hat: float) -> Tuple[torch.FloatTensor, int]:
+    def compute_adaptive_prediction_set(predictions: torch.FloatTensor, q_hat: torch.FloatTensor) -> Tuple[torch.FloatTensor, int]:
         """
         Compute the adaptive prediction set based on the predictions and the quantile.
 
@@ -204,16 +205,30 @@ class ConformalCalibrator:
         Tuple[torch.FloatTensor, int]
             Prediction set (in the form of a zeroed-out and re-normalized output distribution) and its size.
         """
-        # TODO: Vectorize q_hat
-        sorted_classes, index = torch.sort(-predictions)
+        sorted_classes, index = torch.sort(-predictions)  # Sort descendingly
         sorted_probs = torch.gather(predictions, -1, index)
         cum_probs = torch.cumsum(sorted_probs, dim=-1)
+        q_hat = q_hat.unsqueeze(-1).repeat(1, predictions.shape[-1])
 
+        # Because adaptive prediction sets are supposed to include one more class than the ones that would come out of
+        # the comparisons between the cumulative probabilities and q_hat (to avoid empty predictions sets), we adjust
+        # q_hat by adding the difference to the next cumulative probability that would otherwise not be included.
+        diffs = cum_probs - q_hat  # The value we are looking for here will be the smallest positive value per row
+        diffs[diffs <= 0] = 1  # Second: Set all 0s to 1 to make sure that we don't pick them
+        offset_values = torch.min(diffs, dim=-1, keepdim=True)[0]  # Third: Find the smallest positive value per row
+        offset_values += 5e-3  # Add small value so that we include the minimum value as well
+        # (The value has a strange value to accommodate errors caused floating point precision)
+        q_hat[torch.isinf(q_hat)] = 0  # Remove infinity values, these will be replaced by > 1, so functionally the same
+        q_hat += offset_values
+
+        # Compute set sizes
+        set_sizes = torch.sum((cum_probs < q_hat).long(), -1).numpy()
+
+        # Compute actual prediction sets
         # Adapted from https://stackoverflow.com/questions/52127723/pytorch-better-way-to-get-back-original-tensor-order
         # -after-torch-sort
-        set_size = torch.sum((cum_probs < q_hat).long()).numpy()
         unsorted_cum_probs = torch.gather(cum_probs, -1, index.argsort(-1))
-        predictions[unsorted_cum_probs < q_hat] = 0
-        predictions /= predictions.sum()
+        predictions[unsorted_cum_probs >= q_hat] = 0
+        predictions /= predictions.sum(-1, keepdim=True)
 
-        return predictions, set_size
+        return predictions, set_sizes

src/datastore.py
@@ -87,7 +87,7 @@ class DataStore:
         if use_quantization:
             quantizer = faiss.IndexFlatIP(self.key_dim)
             self.index = faiss.IndexIVFPQ(quantizer, self.key_dim, self.num_centroids, self.code_size, 8)
-            self.index.nprobe = 32
+            self.index.nprobe = num_probes
 
         else:
             self.index = faiss.IndexFlatIP(self.key_dim)
@@ -110,9 +110,7 @@ class DataStore:
             Directory containing index.trained (the trained index) and token_ids.pt (token ids sorted by index id).
         """
         self.index = faiss.read_index(os.path.join(save_dir, self.index_file_name))
-        self.value_tensor = torch.tensor(
-            torch.load(os.path.join(save_dir, self.token_ids_file_name))
-        )
+        self.value_tensor = torch.load(os.path.join(save_dir, self.token_ids_file_name))
 
     def train_index(self, key_data: torch.FloatTensor, max_training_keys: int = 1000000) -> None:
         """
@@ -161,16 +159,21 @@ class DataStore:
         except Exception as e:
             raise IOError(f"Encountered error when saving torch tensor to {output_dir}: {e}")
 
-    def search_k(self, query: torch.tensor, k: int) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+    def search_k(self, query: torch.FloatTensor, k: int) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
         """
         Search for the top K nearest neighbors, along with the distance.
         :param k: top k
         :param query: should have shape (num_queries, dim_keys).
         :return: scores: should have shape (num_queries, vocab_size), contains scores for each token for each entry
         """
+        query = query.cpu().numpy()
+
         distances, indices = self.index.search(
             query, k
         )  # D, I will have shape (num_queries, k), containing the distance and the index
+
+        distances, indices = torch.FloatTensor(distances), torch.LongTensor(indices)
+
         values = self.value_tensor[indices, :]  # (num_queries, k)
 
         return distances, values