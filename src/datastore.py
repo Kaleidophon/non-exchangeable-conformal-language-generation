d569670f18405ec4d07c82218a83dee3df692209
Kaleidophon <dennis.ulmer@gmx.de>
2023-05-18 16:18:20 +0100

    :construction: Add info about index loading

--- src/datastore.py
+++ src/datastore.py
@@ -110,7 +110,9 @@ class DataStore:
             Directory containing index.trained (the trained index) and token_ids.pt (token ids sorted by index id).
         """
         self.index = faiss.read_index(os.path.join(save_dir, self.index_file_name))
+        print(f"Loaded index with {self.index.ntotal} entries from disk.")
         self.value_tensor = torch.load(os.path.join(save_dir, self.token_ids_file_name)).to(self.device)
+        print(f"Loaded {self.value_tensor.shape[0]} values from disk.")
 
     def train_index(self, key_data: torch.FloatTensor, max_training_keys: int = 1000000) -> None:
         """
