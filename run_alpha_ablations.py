ea717d447045c69f0cd7123b6b9b7c82eced495b
Kaleidophon <dennis.ulmer@gmx.de>
2023-10-11 16:19:44 +0200

    :bug: Fix CL arg for alpha ablation

--- run_alpha_ablations.py
+++ run_alpha_ablations.py
@@ -319,7 +319,8 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--alpha-values",
-        type=list,
+        type=float,
+        nargs="+",
         default=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     )
     parser.add_argument(
